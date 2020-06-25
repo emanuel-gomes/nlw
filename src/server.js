910ceb52191324c9b5ce0550a24f29aa04676a63
Emanuel Gomes <emanuel.gfsdeveloper@gmail.com>
2020-06-24 22:09:09 -0300

    2 anos em 2 meses concluído

--- src/server.js
+++ src/server.js
@@ -1,8 +1,27 @@
 const express = require("express")
 const server = express()
 
+//configurar pasta publica
+server.use(express.static("public"))
+
+
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
 server.get("/", (req, res) => {
-    res.send("cheguei aqui")
+    return res.render("index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
 })
 
 server.listen(3000)
